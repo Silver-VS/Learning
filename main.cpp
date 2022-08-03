cc41ab0b762c8719334d13f3a28411d79ce1643a
Silver VS <silver.vs@outlook.com>
2022-08-03 01:25:05 -0500

    Exercise 1-1 to 1-3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+using namespace std;
+
+/*
+ * Create a function called absolute_value that returns the absolute value of
+ * its single argument. The absolute value of an integer x is the following: x (itself)
+ * if x is greater than or equal to 0; otherwise, it is x times −1
+ */
+int absolute_value(int x) {
+    return x < 0 ? (x) * (-1) : x;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    printf("Hello, world! from printF\n");
+    cout << "Hello, World from cout with std" << endl;
+    std::cout << "Hello, World! from std::cout" << std::endl;
+    int my_num = -3567;
+    printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
 }
