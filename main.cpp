5ec4c1700a712a1bbbdccc9f939ec0b24366802b
Silver VS <silver.vs@outlook.com>
2022-08-03 01:28:49 -0500

    Exercise 1-4

--- main.cpp
+++ main.cpp
@@ -11,10 +11,22 @@ int absolute_value(int x) {
     return x < 0 ? (x) * (-1) : x;
 }
 
+/*
+ * Write another function called sum that takes two int arguments and returns
+ * their sum. How can you modify the template in Listing 1-9 to test your new
+ * function?
+ */
+int sum(int x, int y){
+    return x + y;
+}
+
 int main() {
     printf("Hello, world! from printF\n");
     cout << "Hello, World from cout with std" << endl;
     std::cout << "Hello, World! from std::cout" << std::endl;
     int my_num = -3567;
     printf("The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
+    int first_add = 5;
+    int second_add = 6;
+    printf("The sum of %d and %d is equal to %d", first_add, second_add, sum(first_add, second_add));
 }
